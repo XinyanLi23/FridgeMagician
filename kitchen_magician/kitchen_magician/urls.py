38eae7354ef4ec758137a5389f2431ec072e911a
Xinyan Li <tutu@xinyans.attlocal.net>
2020-10-30 10:48:28 -0500

    users login

--- kitchen_magician/kitchen_magician/urls.py
+++ kitchen_magician/kitchen_magician/urls.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from django.urls import include
 from search import views as search_views
-
+from users import views as login_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('search/', search_views.search, name='search'),
+    path('login/', login_views.login, name='login'),
     path('', include('km.urls')),
     path('about/', include('about.urls')),
 ]
--- kitchen_magician/users/views.py
+++ kitchen_magician/users/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    context = {
+        'title': 'LOGIN'
+    }
+    return render(request, 'login.html', context)
